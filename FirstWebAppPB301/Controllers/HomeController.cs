c4331585579f9567056d14073388e1b4d2915fe1
emilmv <emilnm@code.edu.az>
2024-07-21 16:13:49 +0400

    Add project files.

---
+++ FirstWebAppPB301/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using FirstWebAppPB301.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebAppPB301.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return File("~/img/Road_in_Norway.jpg","image/jpg");
+            //return View();
+            return Json(new Product
+            {
+                Id = 1,
+                Name = "Test",
+            });
+        }
+        public IActionResult About()
+        {
+            return View();
+            //return Content("First WebApp of About");
+        }
+        public IActionResult Contact()
+        {
+            //return View();
+            return Content("First WebApp of Contact");
+        }
+    }
+
+}
---
+++ FirstWebAppPB301/Models/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace FirstWebAppPB301.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

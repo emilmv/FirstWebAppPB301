e8d0a497d7a968ef811723387349f5277da12db9
emilmv <emilnm@code.edu.az>
2024-07-21 18:32:32 +0400

    Added ViewModel

--- FirstWebAppPB301/Controllers/HomeController.cs
+++ FirstWebAppPB301/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirstWebAppPB301.Models;
+using FirstWebAppPB301.ViewModels;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FirstWebAppPB301.Controllers
 {
@@ -12,11 +14,22 @@ namespace FirstWebAppPB301.Controllers
             //Id = 1,
             //Name = "Test",
             //});
-            ViewData["name"] = "Emil";
-            ViewBag.age = 27;
-            TempData["address"] = "Baki";
-            //return View();
-            return RedirectToAction(nameof(About));
+            //ViewData["name"] = "Emil";
+            //ViewBag.age = 27;
+            //TempData["address"] = "Baki";
+            //return RedirectToAction(nameof(About));
+            List<Student> students = new List<Student>(){
+                new(){Id = 1,Name="Student1"},
+                new(){Id = 2,Name="Student2"},
+                new(){Id = 3,Name="Student3"},
+                new(){Id = 4,Name="Student4"}
+            };
+            ViewBag.Students = students;
+            HomeVM homeVM = new HomeVM();
+            homeVM.GroupName = "PB301";
+            homeVM.Student=students[0];
+            homeVM.Students = students;
+            return View(homeVM);
         }
         public IActionResult About()
         {
---
+++ FirstWebAppPB301/ViewModels/HomeVM.cs
@@ -0,0 +1,11 @@
+﻿using FirstWebAppPB301.Models;
+
+namespace FirstWebAppPB301.ViewModels
+{
+    public class HomeVM
+    {
+        public string GroupName { get; set; }
+        public Student Student { get; set; }
+        public List<Student> Students { get; set; }
+    }
+}
